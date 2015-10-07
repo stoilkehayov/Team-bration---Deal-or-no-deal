9c8d55f9f8716657a1ad65f07fe50953614a8ddf
Martin Nikolov <martin.nik@live.com>
2015-10-07 20:47:29 +0300

    ka4eno
    aa

--- Program.cs
+++ Program.cs
@@ -252,26 +252,31 @@ namespace DealOrNoDeal
 
 
 
-            static void Main()
+        static void Main()
         {
-            int[] boxes =
-            {
-                1,5,10,20,50,100,200,300,500,750,1000,2500,5000,7500,10000,12500,15000,25000,50000,100000
-            };
-           
-            Random random = new Random();
-            for (int i = 0; i < boxes.Length; i++)
-            {
-                int index = random.Next(0, boxes.Length);
-                int index2 = random.Next(0, boxes.Length);
-                int temp = boxes[index2];
-                boxes[index2] = boxes[index];
-                boxes[index] = temp;
-               
-            }
-            Console.WriteLine(String.Join(",",boxes));
+            int index3 = 0;
 
-            Console.WriteLine();
+
+
+            List<int> boxesList = new List<int>
+            {
+                1,5,10,20,50,100,200,300,500,750,1000,2500,5000,7500,10000,12500,15000,25000,50000,75000,100000
+            };
+            Random random = new Random();
+            for (int i = 0; i < boxesList.Count; i++)
+            {
+                int index = random.Next(0, boxesList.Count);
+                int index2 = random.Next(0, boxesList.Count);
+                int temp = boxesList[index2];
+                boxesList[index2] = boxesList[index];
+                boxesList[index] = temp;
+            }
+
+
+
+
+
+
         }
     }
 }
